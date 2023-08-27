510816edc91c10a94bd349a74d180a454d965aba
Jack Maloney <jack@maloney.ai>
2023-08-27 17:58:17 -0500

    Update default.nix

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  roon-server = pkgs.callPackage ./pkgs/roon-server { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
