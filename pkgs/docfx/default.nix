4aabd9f69df14af9b3dab8338b28fa4d1c9e2a40
Jack Maloney <jack@maloney.ai>
2024-01-15 18:01:08 -0600

    fix playwright permissions

--- pkgs/docfx/default.nix
+++ pkgs/docfx/default.nix
@@ -6,12 +6,18 @@
   fetchNuGet,
   dotnetCoreSdk ? dotnetCorePackages.sdk_7_0,
 }: let
-  nuget = fetchNuGet {
-    pname = "docfx";
-    version = "2.75.1";
-    sha256 = "sha256-bbD4+yNxM4vmZXPczeFH+Hy5IohKVA2cIrb+88tLD8Y=";
-    outputFiles = ["*" ];
-  };
+  nuget =
+    (fetchNuGet {
+      pname = "docfx";
+      version = "2.75.1";
+      sha256 = "sha256-bbD4+yNxM4vmZXPczeFH+Hy5IohKVA2cIrb+88tLD8Y=";
+      outputFiles = ["*"];
+    })
+    .overrideAttrs (old: {
+      postUnpack = ''
+        chmod +x tools/.playwright/node/*/playwright.sh
+      '';
+    });
 in
   writeShellScriptBin "docfx" ''
     ${lib.getExe dotnetCoreSdk} ${nuget}/lib/dotnet/docfx/tools/net${lib.versions.majorMinor dotnetCoreSdk.version}/any/docfx.dll "$@";
