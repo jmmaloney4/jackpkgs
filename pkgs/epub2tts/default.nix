da14d9588373b5c9a549adaad1f00b9a0be33b01
Jack Maloney <jack@maloney.ai>
2024-03-31 19:37:22 -0500

    update dependencies

--- pkgs/epub2tts/default.nix
+++ pkgs/epub2tts/default.nix
@@ -38,25 +38,25 @@ in
         pname = "deepspeed";
         version = "0.14.0";
         sha256 = "sha256-ihQLaT+Zl3ZjfvAEURJDR5eV9tPaLj+SwdUIt1YYORc=";
-	propagatedBuildInputs = [pip];
+        propagatedBuildInputs = [pip];
       };
       edge-tts = pypiPackage {
         pname = "edge-tts";
         version = "6.1.10";
         sha256 = "sha256-cKSfMu12bqQFuNKkTvEkgFNJ0pbBpWoiCwr/HiAviJE=";
-	propagatedBuildInputs = [pip certifi];
+        propagatedBuildInputs = [pip certifi];
       };
       newspaper3k = pypiPackage {
         pname = "newspaper3k";
         version = "0.2.8";
         sha256 = "sha256-nxvT4ftI9ADHFav4dcx7Cme33c2H9Qya7rj8u72QBPs=";
-	propagatedBuildInputs = [pip];
+        propagatedBuildInputs = [pip];
       };
       noisereduce = pypiPackage {
         pname = "noisereduce";
         version = "3.0.2";
         sha256 = "sha256-ClMtIiOYboKVrleo+G+0aZpHdjaEEjf/qMMjyVm9nAs=";
-	propagatedBuildInputs = [pip tqdm numpy];
+        propagatedBuildInputs = [pip tqdm numpy];
       };
       pedalboard = buildPythonPackage {
         name = "pedalboard";
@@ -66,8 +66,19 @@ in
           rev = "c3f44ba7740a629a11298ad90ec20602f25d3fdd";
           sha256 = "sha256-MIJ9BpqnG5SoyO/mKn7NpLJV2e2cwNvJ/LK+A/jUGRQ=";
         };
-	propagatedBuildInputs = [pybind11 pkgconfig pkg-config-unwrapped];
-	buildInputs = [pkg-config-unwrapped];
+        propagatedBuildInputs = [
+          pybind11
+          pkgconfig
+          (freetype-py.overridePythonAttrs
+            (old: {
+              propagatedBuildInputs =
+                old.propagatedBuildInputs
+                ++ [
+                  pkg-config-unwrapped
+                ];
+            }))
+          pkg-config-unwrapped
+        ];
       };
       TTS = pypiPackage {
         pname = "TTS";
