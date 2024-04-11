6be8a6eb25444d6428be6a04d8814b231bb909bc
Jack Maloney <jack@maloney.ai>
2024-04-11 12:22:32 -0500

    Switch to dream2nix and get update csharpier

--- flake.nix
+++ flake.nix
@@ -1,28 +1,104 @@
 {
-  description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    ### Nixpkgs ###
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    ### Flake / Project Inputs ###
+    dream2nix = {
+      url = "github:nix-community/dream2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    flake-root.url = "github:srid/flake-root";
+
+    mission-control.url = "github:Platonic-Systems/mission-control";
+
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.flake-utils.inputs.systems.follows = "systems";
+    };
+
+    systems.url = "github:nix-systems/default";
+
+    treefmt = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
   outputs = {
     self,
+    dream2nix,
     nixpkgs,
-  }: let
-    systems = [
-      "x86_64-linux"
-      "i686-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-      "aarch64-linux"
-      "armv6l-linux"
-      "armv7l-linux"
-    ];
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-  in {
-    legacyPackages = forAllSystems (system:
-      import ./default.nix {
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
+    flake-parts,
+    flake-root,
+    mission-control,
+    pre-commit-hooks,
+    systems,
+    treefmt,
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} ({
+      withSystem,
+      inputs,
+      ...
+    }: {
+      systems = import systems;
+      imports = [
+        flake-root.flakeModule
+        mission-control.flakeModule
+        pre-commit-hooks.flakeModule
+        treefmt.flakeModule
+      ];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        lib,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            config.mission-control.devShell
+            config.pre-commit.devShell
+            config.treefmt.build.devShell
+          ];
+          buildInputs = with pkgs; [
+          ];
         };
-      });
-    packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
-  };
+
+        mission-control.scripts = {
+          fmt = {
+            description = "Format the source tree";
+            exec = config.treefmt.build.wrapper;
+            category = "Dev Tools";
+          };
+        };
+
+        pre-commit = {
+          check.enable = true;
+          settings.hooks.treefmt.enable = true;
+          settings.settings.treefmt.package = config.treefmt.build.wrapper;
+        };
+
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          package = pkgs.treefmt;
+          programs.alejandra.enable = true;
+        };
+        formatter = config.treefmt.build.wrapper;
+
+        packages = dream2nix.lib.importPackages {
+          projectRoot = ./.;
+          projectRootFile = "flake.nix";
+          packagesDir = ./pkgs;
+          packageSets.nixpkgs = pkgs;
+        };
+      };
+    });
 }
--- pkgs/csharpier/default.nix
+++ pkgs/csharpier/default.nix
@@ -1,18 +1,39 @@
 {
-  stdenv,
+  dream2nix,
+  config,
   lib,
-  dotnetCorePackages,
-  writeShellScriptBin,
-  fetchNuGet,
-  dotnetCoreSdk ? dotnetCorePackages.sdk_7_0,
-}: let
-  nuGet = fetchNuGet {
-    pname = "CSharpier";
-    version = "0.26.7";
-    sha256 = "sha256-QVfbEtkj41/b8urLx8X274KWjawyfgPTIb9HOLfduB8=";
-    outputFiles = ["tools/*"];
+  self,
+  ...
+}:
+with config.deps; let
+  pname = "CSharpier";
+  version = "0.26.7";
+in {
+  imports = [
+    # dream2nix.modules.core.public
+  ];
+
+  name = "csharpier";
+  inherit version;
+
+  deps = {nixpkgs, ...}: {
+    inherit
+      (nixpkgs)
+      stdenv
+      dotnetCorePackages
+      writeShellScriptBin
+      fetchNuGet
+      ;
   };
-in
-  writeShellScriptBin "csharpier" ''
-    ${lib.getExe dotnetCoreSdk} ${nuGet}/lib/dotnet/CSharpier/net${lib.versions.majorMinor dotnetCoreSdk.version}/any/dotnet-csharpier.dll "$@";
-  ''
+
+  public = let
+    nuGet = fetchNuGet {
+      pname = "CSharpier";
+      version = "0.26.7";
+      sha256 = "sha256-QVfbEtkj41/b8urLx8X274KWjawyfgPTIb9HOLfduB8=";
+      outputFiles = ["tools/*"];
+    };
+    dotnetCoreSdk = dotnetCorePackages.sdk_8_0;
+  in
+    writeShellScriptBin "csharpier" ''${lib.getExe dotnetCoreSdk} ${nuGet}/lib/dotnet/CSharpier/net${lib.versions.majorMinor dotnetCoreSdk.version}/any/dotnet-csharpier.dll "$@";'';
+}
