25ad127649d5a7cc0dceb443ad2ebf8994579394
Jack Maloney <jmmaloney4@gmail.com>
2024-12-13 17:26:59 -0600

    fix: roon-update.sh

--- pkgs/roon-server/update.sh
+++ pkgs/roon-server/update.sh
@@ -12,8 +12,6 @@ newHash=${data[0]}
 tarPath=${data[1]}
 newVersion="$(tar -jxvOf $tarPath RoonServer/VERSION 2> /dev/null | head -n 1)"
 
-
 sed --in-place "s/hash = \"sha256\:[a-z0-9]*\"/hash = \"sha256:$newHash\"/g" "$SCRIPTPATH/default.nix"
 sed --in-place "s/version = \"[0-9]*\"/version = \"$newVersion\"/g" "$SCRIPTPATH/default.nix"
-echo "commit-msg=\"[roon] update to $newVersion\"" >> "$GITHUB_OUTPUT"
-
+echo "commit-msg=\"[roon] update to $newVersion\"" 
