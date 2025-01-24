930b7ef6ceee6567861a180dcc8453f92fd8ea96
Jack Maloney <jmmaloney4@gmail.com>
2025-01-23 22:02:39 -0600

    wip lms

--- pkgs/lms/default.nix
+++ pkgs/lms/default.nix
@@ -4,6 +4,7 @@
   buildNpmPackage,
   fetchFromGitHub,
   callPackage,
+  openssh,
   #   copyDesktopItems,
   #   makeDesktopItem,
   #   makeWrapper,
@@ -23,35 +24,37 @@
   # ;
     lmstudio-js = fetchFromGitHub {
     owner = "lmstudio-ai";
-    repo = "lmstudio-js";
+    repo = "lmstudio.js";
     rev = "release-43-c6bb28b";
-    sha256 = "sha256-KLptMinet43NLFHoA1k80w6C0BQcLOGJDEtcaUHAnC8=";
+    fetchSubmodules = true;
+    buildInputs = [
+        openssh
+    ];
+    # sha256 = "sha256-KLptMinet43NLFHoA1k80w6C0BQcLOGJDEtcaUHAnC8=";
     };
 
 
-
-  src = fetchFromGitHub {
-    owner = "lmstudio-ai";
-    repo = "lms";
-    rev = "39af94c1c1ea6c8ab270b973e3a5001fe3a6a816";
-    sha256 = "sha256-KLptMinet43NLFHoA1k80w6C0BQcLOGJDEtcaUHAnC8=";
-  };
+#   src = fetchFromGitHub {
+#     owner = "lmstudio-ai";
+#     repo = "lms";
+#     rev = "39af94c1c1ea6c8ab270b973e3a5001fe3a6a816";
+#     sha256 = "sha256-KLptMinet43NLFHoA1k80w6C0BQcLOGJDEtcaUHAnC8=";
+#   };
 
   npmDepsHash = "sha256-qX/2Wqd4QNSzyzu3a39mc88dKivECmNl1IGlxVSGIG0=";
 
-  lms-common-server = buildNpmPackage {
-    pname = "lms-common-server";
-    version = "0.3.31";
-    src = lmstudio-js;
-    npmRoot = "packages/lms-common-server";
-    npmDepsHash = "sha256-qX/2Wqd4QNSzyzu3a39mc88dKivECmNl1IGlxVSGIG0=";
-  };
+#   lms-common-server = buildNpmPackage {
+#     pname = "lms-common-server";
+#     version = "0.3.31";
+#     src = lmstudio-js;
+#     npmRoot = "packages/lms-cli";
+#     # npmDepsHash = "sha256-qX/2Wqd4QNSzyzu3a39mc88dKivECmNl1IGlxVSGIG0=";
+#   };
 in
   buildNpmPackage rec {
     pname = "lms";
     version = "0.3.31";
-    inherit src npmDepsHash;
-    buildInputs = [
-      lms-common-server
-    ];
+    src = lmstudio-js;
+    npmRoot = "packages/lms-cli";
+    npmDepsHash = "sha256-qX/2Wqd4QNSzyzu3a39mc88dKivECmNl1IGlxVSGIG0=";
   }
