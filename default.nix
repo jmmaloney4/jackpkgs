47acf1632221b6d755e035536103d0dc4ced911f
Jack Maloney <jmmaloney4@gmail.com>
2025-03-18 12:16:44 -0500

    fix: callPackage nbstripout

--- default.nix
+++ default.nix
@@ -15,5 +15,6 @@
   docfx = pkgs.callPackage ./pkgs/docfx {};
   epub2tts = pkgs.callPackage ./pkgs/epub2tts {};
   lean = pkgs.callPackage ./pkgs/lean {};
+  nbstripout = pkgs.callPackage ./pkgs/nbstripout {};
   roon-server = pkgs.callPackage ./pkgs/roon-server {};
 }
