91092a501f0594b5749172d9ad07dfa78a0f5300
Jack Maloney <jmmaloney4@gmail.com>
2025-06-17 22:09:48 -0500

    feat: add default overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,4 +2,14 @@
   # Add your overlays here
   #
   # my-overlay = import ./my-overlay;
+  
+  # Default overlay with all packages from jackpkgs
+  default = self: super: {
+    csharpier = super.callPackage ../pkgs/csharpier {};
+    docfx = super.callPackage ../pkgs/docfx {};
+    epub2tts = super.callPackage ../pkgs/epub2tts {};
+    lean = super.callPackage ../pkgs/lean {};
+    nbstripout = super.callPackage ../pkgs/nbstripout {};
+    roon-server = super.callPackage ../pkgs/roon-server {};
+  };
 }
