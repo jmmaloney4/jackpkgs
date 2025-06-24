2d75327176e9e2d92c50e74ddd565364b545a4aa
Jack Maloney <jmmaloney4@gmail.com>
2025-06-24 15:06:44 -0500

    Filter packages by meta.platforms

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,41 @@
 {pkgs}:
 with pkgs.lib; {
   # Add your library functions here
-  #
-  # hexint = x: hexvals.${toLower x};
+
+  /**
+    Filter an attribute set so that it is returned only when the
+    evaluation `system` is included in `systems`.
+
+    Example:
+
+    ```nix
+    myLib.onlyOnSystems ["x86_64-linux" "aarch64-linux"] pkgs.system {
+      foo = ...;
+    }
+    ```
+    will yield the `foo` attribute only on the listed systems.
+  */
+  onlyOnSystems = systems: system: attrs:
+    optionalAttrs (elem system systems) attrs;
+
+  /**
+    Filter an attribute set of packages using each package's
+    `meta.platforms` field. Packages without a `meta.platforms`
+    specification are kept.
+
+    Example:
+
+    ```nix
+    myLib.filterByPlatforms pkgs.system {
+      foo = pkgs.callPackage ./foo {};
+      bar = pkgs.callPackage ./bar {};
+    }
+    ```
+    will omit any package whose `meta.platforms` list does not
+    include the current system.
+  */
+  filterByPlatforms = system: attrs:
+    filterAttrs (_: pkg:
+      let pls = attrByPath ["meta" "platforms"] platforms.all pkg; in
+        elem system pls) attrs;
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -1,12 +1,16 @@
 {
   # Default overlay with all packages from jackpkgs
-  default = self: super: {
-    csharpier = super.callPackage ../pkgs/csharpier {};
-    docfx = super.callPackage ../pkgs/docfx {};
-    epub2tts = super.callPackage ../pkgs/epub2tts {};
-    lean = super.callPackage ../pkgs/lean {};
-    nbstripout = super.callPackage ../pkgs/nbstripout {};
-    roon-server = super.callPackage ../pkgs/roon-server {};
-    tod = super.callPackage ../pkgs/tod {};
-  };
+  default = self: super:
+    let
+      jackLib = import ../lib { pkgs = super; };
+      packages = {
+        csharpier = super.callPackage ../pkgs/csharpier {};
+        docfx = super.callPackage ../pkgs/docfx {};
+        epub2tts = super.callPackage ../pkgs/epub2tts {};
+        lean = super.callPackage ../pkgs/lean {};
+        nbstripout = super.callPackage ../pkgs/nbstripout {};
+        roon-server = super.callPackage ../pkgs/roon-server {};
+        tod = super.callPackage ../pkgs/tod {};
+      };
+    in jackLib.filterByPlatforms super.system packages;
 }
