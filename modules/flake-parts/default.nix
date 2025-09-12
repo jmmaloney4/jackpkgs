51c590837e67586c6ca85bf066255e200750b771
Jack Maloney <jmmaloney4@gmail.com>
2025-09-12 10:13:36 -0500

    feat: add default flakeModule

--- modules/flake-parts/default.nix
+++ modules/flake-parts/default.nix
@@ -1,5 +1,6 @@
-{inputs, ...}: {
+{config, inputs, ...}: {
   flake = {
+    flakeModule = config.flakeModules.default;
     flakeModules = {
       default = import ./all.nix;
       just = import ./just.nix;
