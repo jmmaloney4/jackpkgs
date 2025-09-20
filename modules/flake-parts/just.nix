e3f203e58f11feb42c59c3c301e4d38be929bb97
Jack Maloney <jmmaloney4@gmail.com>
2025-09-20 16:03:40 -0500

    fix(just): fix justfile type error and indentation
    - Use lib.concatStrings to convert list to string for justfile attribute
    - Ensure consistent indentation for treefmt

--- modules/flake-parts/just.nix
+++ modules/flake-parts/just.nix
@@ -248,21 +248,23 @@ in {
           quarto = {
             enable = cfg.quarto.enable && cfg.quarto.sites != [];
             justfile =
-              [
-                ''
-                  # Build all quarto sites
-                  build-all:
-                    ${lib.concatStringsSep "\n" (map (site: "  ${lib.getExe pcfgQuarto.quartoPackage} build {{site}}") cfg.quarto.sites)}
-                ''
-              ]
-              ++ map (site: ''
-                  # Build {{site}}
-                  build-{{site}}:
-                    ${lib.getExe pcfgQuarto.quartoPackage} build {{site}}
-                  # preview {{site}}
-                  {{site}}:
-                    ${lib.getExe pcfgQuarto.quartoPackage} preview {{site}}
-                '') cfg.quarto.sites;
+              lib.concatStrings (
+                [
+                  ''
+                    # Build all quarto sites
+                    build-all:
+                      ${lib.concatStringsSep "\n" (map (site: "  ${lib.getExe pcfgQuarto.quartoPackage} build {{site}}") cfg.quarto.sites)}
+                  ''
+                ]
+                ++ map (site: ''
+                # Build {{site}}
+                build-{{site}}:
+                  ${lib.getExe pcfgQuarto.quartoPackage} build {{site}}
+                # preview {{site}}
+                {{site}}:
+                  ${lib.getExe pcfgQuarto.quartoPackage} preview {{site}}
+              '') cfg.quarto.sites
+              );
           };
         };
       };
